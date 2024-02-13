f30b60f2a159990af8556f1fef2126df7df37d11
caanyvzdb <mcanyvz1996@gmail.com>
2024-02-13 22:43:56 +0300

    configure

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,7 @@
 import styled from 'styled-components';
-import Contact from './components/Contact';
+import { Contact } from './components/Contact';
+import { Hero } from './components/Hero';
+import './App.css';
 
 const Container = styled.div`
   height: 100vh;
@@ -8,7 +10,8 @@ const Container = styled.div`
   overflow-y: auto;
   scrollbar-width: none;
   color: white;
-  background: url('./img/bg.jpeg');
+  background: url('./assets/img/4.png') no-repeat center;
+  background-size: cover;
   &::-webkit-scrollbar {
     display: none;
   }
@@ -17,6 +20,7 @@ const Container = styled.div`
 function App() {
   return (
     <Container>
+      <Hero />
       <Contact />
     </Container>
   );
