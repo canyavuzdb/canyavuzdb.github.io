fd50f6c6a8567809f6219cf3e3794df0585f484c
caanyvzdb <mcanyvz1996@gmail.com>
2024-02-13 22:43:19 +0300

    added Hero feature

---
+++ src/components/Hero.jsx
@@ -0,0 +1,155 @@
+import React, { Suspense } from 'react';
+import styled from 'styled-components';
+import Navbar from './Navbar';
+import { Canvas } from '@react-three/fiber';
+import { OrbitControls, Sphere, MeshDistortMaterial } from '@react-three/drei';
+
+const Section = styled.div`
+  height: 100vh;
+  scroll-snap-align: center;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: space-between;
+  @media only screen and (max-width: 768px) {
+    height: 200vh;
+  }
+`;
+
+const Container = styled.div`
+  height: 100vh;
+  scroll-snap-align: center;
+  display: flex;
+  width: 1400px;
+  justify-content: space-between;
+  @media only screen and (max-width: 768px) {
+    width: 100%;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+  }
+`;
+const Desc = styled.p`
+  font-size: 24px;
+  color: lightgray;
+  @media only screen and (max-width: 768px) {
+    padding: 20px;
+    text-align: center;
+  }
+`;
+
+const Left = styled.div`
+  display: flex;
+  flex: 2;
+  flex-direction: column;
+  justify-content: center;
+  gap: 20px;
+  @media only screen and (max-width: 768px) {
+    flex: 1;
+    align-items: center;
+  }
+`;
+const Title = styled.h1`
+  font-size: 74px;
+
+  @media only screen and (max-width: 768px) {
+    font-size: 60px;
+  }
+`;
+const WhatIDo = styled.div`
+  display: flex;
+  align-items: center;
+  gap: 10px;
+`;
+const Line = styled.img`
+  height: 5px;
+`;
+const Subtitle = styled.h2`
+  color: gray;
+`;
+
+const Button = styled.button`
+  background-color: gray;
+  color: white;
+  font-weight: 500;
+  width: 100px;
+  padding: 10px;
+  border: none;
+  border-radius: 5px;
+  cursor: pointer;
+`;
+
+const Right = styled.div`
+  flex: 3;
+  position: relative;
+  @media only screen and (max-width: 768px) {
+    flex: 1;
+    width: 100%;
+  }
+`;
+const Img = styled.img`
+  width: 600px;
+  height: 500px;
+  object-fit: contain;
+  position: absolute;
+  top: 0;
+  bottom: 100px;
+  left: 0;
+  right: 0;
+  margin: auto;
+  animation: animate 2s infinite ease alternate;
+  @media only screen and (max-width: 768px) {
+    width: 300px;
+    height: 300px;
+  }
+
+  @keyframes animate {
+    to {
+      transform: translateY(20px);
+    }
+  }
+`;
+export const Hero = () => {
+  return (
+    <Section>
+      <Navbar />
+      <Container>
+        <Left>
+          <Title>Mehmet Can Yavuz</Title>
+          <WhatIDo>
+            <Line src="./assets/img/line.png" />
+            <Subtitle>What I Do</Subtitle>
+          </WhatIDo>
+          <Desc>
+            A Full-Stack Developer confidently navigating my way through the
+            field. Based in Turkey, I bring a total of 1 year of experience to
+            the table. Throughout my 1-year journey, I've contributed to a
+            variety of projects and embraced different technologies. This
+            experience has sharpened my problem-solving abilities, enhanced my
+            coding skills, and strengthened my teamwork capabilities. I eagerly
+            anticipate taking on larger, more complex projects in the future.
+          </Desc>
+          <Button>Learn More</Button>
+        </Left>
+        <Right>
+          <Canvas>
+            <Suspense fallback={null}>
+              <OrbitControls enableZoom={false} />
+              <ambientLight intensity={1} />
+              <directionalLight position={[3, 2, 1]} />
+              <Sphere args={[1, 100, 200]} scale={2.4}>
+                <MeshDistortMaterial
+                  color="#C7C8CC"
+                  attach="material"
+                  distort={0.5}
+                  speed={2}
+                />
+              </Sphere>
+            </Suspense>
+          </Canvas>
+          <Img src="./assets/img/8.png" />
+        </Right>
+      </Container>
+    </Section>
+  );
+};
