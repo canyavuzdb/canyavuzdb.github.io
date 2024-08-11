c4859ee91930a4d4b0778468fa7690bef7d25149
Can Yavuz <88374853+canyavuzdb@users.noreply.github.com>
2024-08-11 17:58:13 +0300

    update links with icons

--- portfolio/lib/data.ts
+++ portfolio/lib/data.ts
@@ -1,26 +1,35 @@
+import { IconType } from 'react-icons';
+import { AiOutlineHome, AiOutlineUser, AiOutlineProject, AiOutlineTool, AiOutlineHistory, AiOutlineMessage } from 'react-icons/ai';
+
 export const links = [
   {
     name: "Home",
     hash: "#home",
+    icon: AiOutlineHome,
   },
   {
     name: "About",
     hash: "#about",
+    icon: AiOutlineUser,
   },
   {
     name: "Projects",
     hash: "#projects",
+    icon: AiOutlineProject,
   },
   {
     name: "Skills",
     hash: "#skills",
+    icon: AiOutlineTool,
   },
   {
     name: "Experience",
     hash: "#experience",
+    icon: AiOutlineHistory,
   },
   {
     name: "Contact",
     hash: "#contact",
+    icon: AiOutlineMessage,
   },
 ] as const;
