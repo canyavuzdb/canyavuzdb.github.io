7494242dd1b47e5734edff5df802161869bc7754
Can Yavuz <88374853+canyavuzdb@users.noreply.github.com>
2024-08-16 17:01:55 +0300

    Summary - Update data and added projects worked on

--- portfolio/lib/data.ts
+++ portfolio/lib/data.ts
@@ -1,5 +1,14 @@
-import { IconType } from 'react-icons';
-import { AiOutlineHome, AiOutlineUser, AiOutlineProject, AiOutlineTool, AiOutlineHistory, AiOutlineMessage } from 'react-icons/ai';
+import { IconType } from "react-icons";
+import {
+  AiOutlineHome,
+  AiOutlineUser,
+  AiOutlineProject,
+  AiOutlineTool,
+  AiOutlineHistory,
+  AiOutlineMessage,
+} from "react-icons/ai";
+import jamImg from "@/public/jam.png";
+import webautoImg from "@/public/webautomation.png";
 
 export const links = [
   {
@@ -31,5 +40,24 @@ export const links = [
     name: "Contact",
     hash: "#contact",
     icon: AiOutlineMessage,
+  },
+] as const;
+
+export const Experience = [{}] as const;
+
+export const projectsData = [
+  {
+    title: "Natural Jam & Marmalade Store",
+    description:
+      "A startup project currently underway, a B2B project, aims to list more than 40 products to users.",
+    tags: ["Docker", ".Net", "Microservice", "RabbitMq", "PostgreSQL",],
+    imageUrl: jamImg,
+  },
+  {
+    title: "Ekici Web Automation",
+    description:
+      "I worked as a full-stack developer on this automation project for 4 months. A multifunctional internal software for company employees.",
+    tags: ["React", "Next.js", "Sql", "Tailwind", "Prisma"],
+    imageUrl: webautoImg,
   },
 ] as const;
