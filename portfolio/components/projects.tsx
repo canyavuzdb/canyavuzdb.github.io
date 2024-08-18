070de0ba57ab609c4638cb9cb2fcfdddc22b3710
Can Yavuz <88374853+canyavuzdb@users.noreply.github.com>
2024-08-18 22:31:16 +0300

    Update projects.tsx

--- portfolio/components/projects.tsx
+++ portfolio/components/projects.tsx
@@ -1,14 +1,25 @@
 "use client";
 
-import React from "react";
+import React, { useEffect } from "react";
 import SectionHeading from "./section-heading";
 import { projectsData } from "@/lib/data";
 import Project from "./project";
+import { useActiveLinkContext } from "@/context/active-link-context";
+import { useInView } from "react-intersection-observer";
 
 
 export default function Projects() {
+  const { ref, inView } = useInView({
+    threshold: 0.5,
+  });
+  const {setActiveLink} = useActiveLinkContext();
+  useEffect(() => {
+    if(inView) {
+      setActiveLink("Projects");
+     }
+  },[inView,setActiveLink]);
   return (
-    <section>
+    <section ref={ref} id="projects" className="scroll-mt-28">
       <SectionHeading>Projects worked on</SectionHeading>
       <div>
         {projectsData.map((project, index) => (
