1104a776b3e867d6bddc4ed3773a9fa7a022c004
Can Yavuz <88374853+canyavuzdb@users.noreply.github.com>
2024-10-23 13:04:41 +0300

    Update page.tsx

--- portfolio/app/page.tsx
+++ portfolio/app/page.tsx
@@ -3,6 +3,7 @@ import SectionDivider from "@/components/section-divider";
 import About from "@/components/about";
 import SmoothCursor from "@/components/smooth-cursor";
 import Projects from "@/components/projects";
+import Skills from "@/components/skills";
 
 export default function Home() {
   return (
@@ -11,6 +12,7 @@ export default function Home() {
     <SectionDivider />
     <About />
     <Projects />
+    <Skills/>
     <SmoothCursor />
     </main>
   );
