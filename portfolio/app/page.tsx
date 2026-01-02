0660ce51e78d660872e44ca3dcdc5f970aea5ef2
Can Yavuz <88374853+canyavuzdb@users.noreply.github.com>
2026-01-03 01:33:26 +0300

    Update page.tsx

--- portfolio/app/page.tsx
+++ portfolio/app/page.tsx
@@ -1,24 +1,40 @@
+"use client";
+
 import Intro from "@/components/intro";
-import SectionDivider from "@/components/section-divider";
 import About from "@/components/about";
-import SmoothCursor from "@/components/smooth-cursor";
 import Projects from "@/components/projects";
 import Skills from "@/components/skills";
 import Experience from "@/components/experience";
 import Contact from "@/components/contact";
 
+import SmoothCursor from "@/components/smooth-cursor";
+import { useActiveLinkContext } from "@/context/active-link-context";
+import { motion, AnimatePresence } from "framer-motion";
 
 export default function Home() {
+  const { activeLink } = useActiveLinkContext();
+
   return (
-    <main className="flex flex-col items-center px-4">
-    <Intro/>
-    <SectionDivider />
-    <About />
-    <Projects />
-    <Skills/>
-    <Experience />
-    {/* <Contact/> */}
-    <SmoothCursor />
+    <main className="h-full text-white relative flex flex-col items-center justify-center">
+        <AnimatePresence mode="wait">
+            <motion.div
+                key={activeLink}
+                initial={{ opacity: 0, scale: 0.98, filter: "blur(10px)" }}
+                animate={{ opacity: 1, scale: 1, filter: "blur(0px)" }}
+                exit={{ opacity: 0, scale: 1.02, filter: "blur(10px)" }}
+                transition={{ duration: 0.4, ease: "easeInOut" }}
+                className="w-[50rem] flex-none h-full flex flex-col items-center justify-center p-4 overflow-y-auto text-center custom-scrollbar"
+            >
+                {activeLink === "Home" && <Intro />}
+                {activeLink === "About" && <About />}
+                {activeLink === "Projects" && <Projects />}
+                {activeLink === "Skills" && <Skills />}
+                {activeLink === "Experience" && <Experience />}
+
+                {activeLink === "Contact" && <Contact />}
+            </motion.div>
+        </AnimatePresence>
+        <SmoothCursor />
     </main>
   );
 }
