af735ecaba28dda0f9b53c5c04449488a8c6b7bd
Can Yavuz <88374853+canyavuzdb@users.noreply.github.com>
2026-01-03 01:32:56 +0300

    Create theme-switch.tsx

---
+++ portfolio/components/theme-switch.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import { BsSun } from "react-icons/bs";
+
+export default function ThemeSwitch() {
+  return (
+    <button className="fixed bottom-10 right-10">
+      <BsSun />
+    </button>
+  );
+}
